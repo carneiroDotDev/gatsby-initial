d89a197c30105f4622746943ff8b377f9dcc9041
Luiz Carneiro <luuizpaulo@gmail.com>
2019-05-28 00:13:59 +0200

    Added dynamic queries for blog pages

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,47 @@
 import React from 'react'
 import Layout from '../components/Layout';
+import { useStaticQuery, graphql, Link } from 'gatsby'
 
-export default function _blog() {
+export default function blog() {
+    const data = useStaticQuery(graphql`
+    query {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+                date
+              }
+              html
+              excerpt
+            }
+          }
+        }
+      }`)
+
+    const fetchPosts = data => {
+        return (
+            <ol>
+                {data.allMarkdownRemark.edges.map( post => (
+                  <Link to={`/blog/${post.node.fields.slug}`} id={`${post.node.fields.slug}`}>
+                    <li>
+                      <h2>{post.node.frontmatter.title}</h2>
+                      <p>{post.node.frontmatter.date}</p>
+                    </li>
+                  </Link>
+                    ))}
+            </ol>
+        )
+    }
+
     return (
         <Layout>
             <h1>Blog</h1>
             <p>Posts will show up here later on.</p>
+            {fetchPosts(data)}
         </Layout>
     )
 }
---
+++ src/templates/blogTemp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../components/Layout';
+
+export default function blogTemp() {
+    return (
+        <Layout>
+            This is the blog template
+        </Layout>
+    )
+}
