c530b56b0b74f4d2f196fc1b3cc570a6b955de26
Luiz Carneiro <luiz.carneiro@sport1.de>
2019-05-29 17:27:31 +0200

    Contentful linked and blog post available

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,36 +5,33 @@ import blogStyles from "../../styles/pagesStyle/blogStyles.module.scss"
 
 export default function blog() {
   const data = useStaticQuery(graphql`
-    query {
-      allMarkdownRemark {
-        edges {
-          node {
-            fields {
-              slug
-            }
-            frontmatter {
-              title
-              date
-            }
-            html
-            excerpt
-          }
+  query {
+    allContentfulBlogPost (sort: {
+      fields: publishedDate,
+      order: ASC
+    }) {
+      edges {
+        node {
+          title
+          slug
+          publishedDate (formatString: "MMMM Do, YYYY")
         }
       }
     }
+  }
   `)
 
   const fetchPosts = data => {
     return (
       <ol className={blogStyles.posts}>
-        {data.allMarkdownRemark.edges.map(post => (
-          <li className={blogStyles.post}>
+        {data.allContentfulBlogPost.edges.map(post => (
+          <li className={blogStyles.post} key={post.node.slug}>
             <Link
-              to={`/blog/${post.node.fields.slug}`}
-              id={`${post.node.fields.slug}`}
+              to={`/blog/${post.node.slug}`}
+              id={`${post.node.slug}`}
             >
-              <h2>{post.node.frontmatter.title}</h2>
-              <p>{post.node.frontmatter.date}</p>
+              <h2>{post.node.title}</h2>
+              <p>{post.node.publishedDate}</p>
             </Link>
           </li>
         ))}
--- src/templates/blogTemp.js
+++ src/templates/blogTemp.js
@@ -1,25 +1,47 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
+// export const query = graphql`
+//   query($slug: String!) {
+//     markdownRemark(fields: { slug: { eq: $slug } }) {
+//       frontmatter {
+//         title
+//         date
+//       }
+//       html
+//     }
+//   }
+// `
 
 export const query = graphql`
   query($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      publishedDate(formatString: "MMMM Do, YYYY")
+      body {
+        json
       }
-      html
     }
   }
 `
-
 export default function blogTemp({ data }) {
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        const alt = node.data.target.fields.title["en-US"]
+        const url = node.data.target.fields.file["en-US"].url
+        return <img src={url} alt={alt} />
+      },
+    },
+  }
+
   return (
     <Layout>
-      <h1>{data.markdownRemark.frontmatter.title}</h1>
-      <p>{data.markdownRemark.frontmatter.date}</p>
-      <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+      <h1> {data.contentfulBlogPost.title} </h1>
+      <p> {data.contentfulBlogPost.publishedDate} </p>
+      {documentToReactComponents(data.contentfulBlogPost.body.json, options)}
     </Layout>
   )
 }
