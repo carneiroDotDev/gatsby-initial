66f93ae54d25287f18f037f2f0e439d7c1789ba8
Luiz Carneiro <luiz.carneiro@sport1.de>
2019-06-05 17:18:44 +0200

    typescript linting rules modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,62 @@
-import React from 'react'
-import { Link, graphql, useStaticQuery } from 'gatsby'
-import headerStyles from '../../styles/componentsStyle/header.module.scss'
+import React from "react"
+import { Link, graphql, useStaticQuery } from "gatsby"
+import headerStyles from "../../styles/componentsStyle/header.module.scss"
 
 export default function Header() {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query {
-        site {
-          siteMetadata {
-            title,
-            author
-          }
+      site {
+        siteMetadata {
+          title
+          author
         }
-      }`)
+      }
+    }
+  `)
 
-    return (
-        <header className={headerStyles.header}>
-            <h1> {data.site.siteMetadata.title} </h1>
-            <nav>
-                <ul className={headerStyles.navList}>
-                    <li>
-                        <Link className={headerStyles.navItem} activeClassName={headerStyles.navItemActive} to="/"> Home </Link>
-                    </li>
-                    <li>
-                        <Link className={headerStyles.navItem} activeClassName={headerStyles.navItemActive} to="/blog"> Blog </Link>
-                    </li>
-                    <li>
-                        <Link className={headerStyles.navItem} activeClassName={headerStyles.navItemActive} to="/about"> About </Link>
-                    </li>
-                    <li>
-                        <Link className={headerStyles.navItem} activeClassName={headerStyles.navItemActive} to="/contact"> Contact </Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header className={headerStyles.header}>
+      <h1> {data.site.siteMetadata.title} </h1>
+      <nav>
+        <ul className={headerStyles.navList}>
+          <li>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.navItemActive}
+              to="/"
+            >
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.navItemActive}
+              to="/blog"
+            >
+              Blog
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.navItemActive}
+              to="/about"
+            >
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={headerStyles.navItem}
+              activeClassName={headerStyles.navItemActive}
+              to="/contact"
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
 }
