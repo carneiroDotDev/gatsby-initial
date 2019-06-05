173283408e20921c89a1d37f613e030a3f7eae60
Luiz Carneiro <luiz.carneiro@sport1.de>
2019-06-05 16:58:00 +0200

    Converted first file to tsx

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Head from "../components/Head"
+import Layout from "../components/Layout"
+
+export default function about(): JSX.Element {
+  return (
+    <Layout>
+      <Head title="About" />
+      <h1>About </h1>
+    </Layout>
+  )
+}
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Layout from '../components/Layout';
 import Head from '../components/Head'
 
-export default function _contact() {
+export default function contact() {
     return (
         <Layout>
             <Head title="contact" />
