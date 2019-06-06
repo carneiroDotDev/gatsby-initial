06de70b501f488224078ba3025c5570bf4ad66ac
Luiz Carneiro <luiz.carneiro@sport1.de>
2019-06-06 09:58:42 +0200

    started typing blog.tsx

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -2,35 +2,29 @@ import React from "react"
 import Layout from "../components/Layout"
 import { useStaticQuery, graphql, Link } from "gatsby"
 import blogStyles from "../../styles/pagesStyle/blogStyles.module.scss"
-import Head from '../components/Head'
+import Head from "../components/Head"
 
-export default function blog() {
-  const data = useStaticQuery(graphql`
-  query {
-    allContentfulBlogPost (sort: {
-      fields: publishedDate,
-      order: ASC
-    }) {
-      edges {
-        node {
-          title
-          slug
-          publishedDate (formatString: "MMMM Do, YYYY")
+export default function blog(): JSX.Element {
+  const content: string = useStaticQuery(graphql`
+    query {
+      allContentfulBlogPost(sort: { fields: publishedDate, order: ASC }) {
+        edges {
+          node {
+            title
+            slug
+            publishedDate(formatString: "MMMM Do, YYYY")
+          }
         }
       }
     }
-  }
   `)
 
-  const fetchPosts = data => {
+  const fetchPosts = (data: string): JSX.Element => {
     return (
       <ol className={blogStyles.posts}>
         {data.allContentfulBlogPost.edges.map(post => (
           <li className={blogStyles.post} key={post.node.slug}>
-            <Link
-              to={`/blog/${post.node.slug}`}
-              id={`${post.node.slug}`}
-            >
+            <Link to={`/blog/${post.node.slug}`} id={`${post.node.slug}`}>
               <h2>{post.node.title}</h2>
               <p>{post.node.publishedDate}</p>
             </Link>
@@ -45,7 +39,7 @@ export default function blog() {
       <Head title="Blog" />
       <h1>Blog</h1>
       <p>Posts will show up here later on.</p>
-      {fetchPosts(data)}
+      {fetchPosts(content)}
     </Layout>
   )
 }
